0dc0a56322a012f482e76dceae5c7dfc62ec2280
yepsamii <abrahmansw@gmail.com>
2026-05-15 00:53:10 +0600

    fix var imports

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,6 @@ if (process.env.NODE_ENV === "production"){
 
 app.listen(PORT, async () => {
     await connectDB();
-    console.log("Server started at http://localhost:5000");
+    console.log(`Server started at http://localhost:${PORT}`);
 });
 
