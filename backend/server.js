9ff8f4aa7cf708681959a8cdc493fb059c5b40ea
Fawaj Suraim <fawajsuraim@gmail.com>
2026-05-14 06:50:22 +0600

    Changes

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,10 @@ import todoRoutes from "./routes/todo.route.js";
 import { connectDB } from "./config/db.js";
 import cors from "cors";
 import path from "path";
-const PORT = process.env.PORT || 5000;
 
 dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -15,7 +16,7 @@ const app = express();
 // });
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 app.use("/api/todos", todoRoutes);
 
@@ -28,8 +29,8 @@ if (process.env.NODE_ENV === "production"){
     });
 }
 
-app.listen(PORT, () => {
-    connectDB();
+app.listen(PORT, async () => {
+    await connectDB();
     console.log("Server started at http://localhost:5000");
 });
 
