768443d8cdcaa84e80bcf1d8a215ff94b2232546
yepsamii <abrahmansw@gmail.com>
2026-05-15 01:22:44 +0600

    update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,8 +8,10 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://localhost:4000",
       },
     },
+
+    open: true,
   },
 });
